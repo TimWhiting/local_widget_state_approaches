1c1754861a88c6756acee6dea51a701886a36c52
Tim Whiting <tim@whitings.org>
2020-08-06 18:50:33 -0600

    inital commit of counter in hooks + stateful

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:local_widget_state_approaches/hooks/counter.dart';
+import 'package:local_widget_state_approaches/stateful/counter.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,107 +14,66 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class HomePage extends HookWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+    final approach = useState(Approach.Hooks);
+    final example = useState(Examples.Counter);
+    return Row(
+      children: [
+        NavigationRail(
+          destinations: [
+            ...Examples.values.map(
+              (a) => NavigationRailDestination(
+                  icon: Container(), label: Text(a.toString().split('.')[1])),
             ),
           ],
+          selectedIndex: Examples.values.indexOf(example.value),
+          onDestinationSelected: (i) => example.value = Examples.values[i],
+          labelType: NavigationRailLabelType.all,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        NavigationRail(
+          destinations: [
+            ...Approach.values.map(
+              (a) => NavigationRailDestination(
+                  icon: Container(), label: Text(a.toString().split('.')[1])),
+            ),
+          ],
+          selectedIndex: Approach.values.indexOf(approach.value),
+          onDestinationSelected: (i) => approach.value = Approach.values[i],
+          labelType: NavigationRailLabelType.all,
+        ),
+        Expanded(child: example.value.pageFor(approach.value)),
+      ],
     );
+  }
+}
+
+enum Approach { Hooks, Stateful, LateProperty }
+
+enum Examples { Counter, SomethingElse }
+
+extension on Examples {
+  Widget pageFor(Approach approach) {
+    switch (this) {
+      case Examples.Counter:
+        switch (approach) {
+          case Approach.Hooks:
+            return CounterHooks('Hook Counter');
+          case Approach.Stateful:
+            return StatefulCounter(title: 'Stateful Counter');
+          default:
+            return StatefulCounter(title: 'Stateful Counter');
+        }
+    }
+    return CounterHooks('Hook Counter Default');
   }
 }
